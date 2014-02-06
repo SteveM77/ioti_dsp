b829bfbf068d5984b7e2f22061699772ed3ced67
SteveM <none@none>
2014-02-05 20:50:34 -0500

    Original ips project from Marc with exports moved to own directory.

---
+++ ips/Export/defines.h
@@ -0,0 +1,29 @@
+/*
+ * File:           defines.h
+ *
+ * Created:        Thursday, November 07, 2013 3:42:12 PM
+ * Description:    ipsEQ IC default download data definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2013 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+#define BufferSize_ADAU1445     	20284
+#define NumTransactions_ADAU1445	22
+
+#define BufferSize_ADAU1966     	96
+#define NumTransactions_ADAU1966	32
+
+
+#endif
---
+++ ips/Export/ipsEQ_ADAU1966_PARAM.h
@@ -0,0 +1,23 @@
+/*
+ * File:           Y:\BEA\ioti\software\SigmaDSP\ips\ipsEQ_ADAU1966_PARAM.h
+ *
+ * Created:        Thursday, November 07, 2013 3:42:12 PM
+ * Description:    ipsEQ:ADAU1966 parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2013 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __IPSEQ_ADAU1966_PARAM_H__
+#define __IPSEQ_ADAU1966_PARAM_H__
+
+
+#endif
