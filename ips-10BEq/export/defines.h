a012a1b0accecd8e265215a1d32461749c9b5b25
SteveM <none@none>
2014-02-16 16:53:19 -0500

    Added 10 band EQ version.

---
+++ ips-10BEq/export/defines.h
@@ -0,0 +1,29 @@
+/*
+ * File:           defines.h
+ *
+ * Created:        Sunday, February 16, 2014 4:50:02 PM
+ * Description:    ips-10BEQ IC default download data definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2014 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+#define BufferSize_ADAU1445     	9920
+#define NumTransactions_ADAU1445	25
+
+#define BufferSize_ADAU1966     	102
+#define NumTransactions_ADAU1966	34
+
+
+#endif
---
+++ ips-10BEq/export/ips-10BEQ_ADAU1966_PARAM.h
@@ -0,0 +1,23 @@
+/*
+ * File:           C:\Users\mobleys\Documents\Analog Devices\SigmaStudio 3.10\Projects\ioti_DSP\ips-10BEq\export\ips-10BEQ_ADAU1966_PARAM.h
+ *
+ * Created:        Sunday, February 16, 2014 4:50:02 PM
+ * Description:    ips-10BEQ:ADAU1966 parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2014 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __IPS_10BEQ_ADAU1966_PARAM_H__
+#define __IPS_10BEQ_ADAU1966_PARAM_H__
+
+
+#endif
